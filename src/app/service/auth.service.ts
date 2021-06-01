9ae4b50001681ce4cc8a43623760ffe5fa997299
hashtagmath <matheussilvadossantos0@gmail.com>
2021-06-01 15:34:09 -0300

    Componente inicio sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { CadastrarComponent } from '../cadastrar/cadastrar.component';
 import { EntrarComponent } from '../entrar/entrar.component';
 import { User } from '../model/User';
@@ -23,4 +24,13 @@ cadastrar(user: User): Observable<User>{
 return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
   
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok 
+  }
 }
